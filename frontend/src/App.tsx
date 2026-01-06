8e65afcfed7c88c84232de23967476a4d410aff5
Abhiravkul <k99abhirav@gmail.com>
2026-01-07 00:22:49 +0530

    Initialize React frontend project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-xl font-semibold">
+        Wallet Service Frontend
+      </h1>
+    </div>
+  );
+}
+
+export default App
---
+++ frontend/src/api/client.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export const apiClient = axios.create({
+    baseURL: "http://localhost:3000",
+    timeout: 5000,
+    headers: {
+        "Content-Type": "application/json",
+    },
+})
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      // backend returned an error payload
+      return Promise.reject(error.response.data);
+    }
+
+    // network / timeout / CORS / server down
+    return Promise.reject({
+      error: "Network error",
+    });
+  }
+);
